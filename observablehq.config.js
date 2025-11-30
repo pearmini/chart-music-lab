c23368531df38dedab92aa67eba90de685f858e5
pearmini <subairui@icloud.com>
2025-11-30 08:43:38 -0500

    Clean url

--- observablehq.config.js
+++ observablehq.config.js
@@ -21,8 +21,6 @@ export default {
   // The path to the source root.
   root: "src",
 
-  preserveExtension: true,
-
   // Some additional configuration options and their defaults:
   theme: "dark", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
